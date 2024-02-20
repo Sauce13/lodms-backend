ba509cd8264c29f68f7b93505d567bb66c2d0e74
Adeel Ahmed <leedademha@gmail.com>
2024-02-20 09:17:12 -0800

    Removed cors restriction, generated course scaffold

---
+++ db/migrate/20240220171322_create_courses.rb
@@ -0,0 +1,13 @@
+class CreateCourses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :crn
+      t.string :subject-area
+      t.string :instructor
+      t.string :college
+
+      t.timestamps
+    end
+  end
+end
